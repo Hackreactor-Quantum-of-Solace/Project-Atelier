791876fe3c254d33040f3320131b5f56078dd299
MA Hereford <bluechocolate2019@gmail.com>
2022-11-30 14:14:01 -0800

    created RatingsReview folder and Review, ReviewList components

---
+++ client/src/components/RatingsReview/Review.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//maps through all the reviews and formats them into 'tiles'
+export default class Review extends React.Component {
+
+
+  render () {
+    let reviewsArr = ["Comfortable and practical.", "They are very dark. But that's good because I'm in very sunny spots"]
+
+    let reviewList = reviewsArr.map( (review, key) => {
+     console.log(review, key)
+     return <li>{review}</li>
+    })
+    return (
+      <div>
+       <ul>
+        {reviewList}
+       </ul>
+      </div>
+    )
+  }
+}
+
