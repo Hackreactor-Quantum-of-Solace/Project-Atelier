197a38e4285e2cf8c6c848fd72f9139926923574
Nate Foster <nate.s.foster@gmail.com>
2022-12-01 11:39:01 -1000

    Install and use morgan library for logging server requests

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 
 const port = process.env.PORT || 3000;
