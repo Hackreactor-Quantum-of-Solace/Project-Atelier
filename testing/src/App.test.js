ef5d0068700b0bf95bb4475b66a53dd10b0dc8c8
MBellinghausen <michaelangelobellinghausen@gmail.com>
2022-12-01 23:49:46 -0600

    added documentation to readme.md and removed the snapshot feature of dummy test

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,16 +1,17 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import render from "react-test-renderer";
 import App from "/home/sprung/hackreactor/Project-Atelier/client/src/components/App.jsx";
 
 
 describe("App Component", function () {
   it("should have hello world message", function () {
     console.log(App);
-    const component = renderer.create(
-      <App page="http://www.facebook.com">Facebook</App>,
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    console.log(render.create(<App />));
+    // const component = renderer.create(
+    //   <App page="http://www.facebook.com">Facebook</App>,
+    // );
+    // let tree = component.toJSON();
+    // expect(tree).toMatchSnapshot();
     // let getByText = renderer(<App />);
     // expect(getByText("Hello World")).toMatchInlineSnapshot(`
     //   <h1>
