af34192d5ec8b70a6bec275ce5dd9b7b458fcdd1
Nate Foster <nate.s.foster@gmail.com>
2022-12-08 09:31:12 -1000

    Build quantity selector and allow quanity change for current size

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -8,23 +8,37 @@ export default class AddToCart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      current_sku: ''
+      current_sku: '',
+      current_size: '',
+      current_quantity: 0
     }
     this.selectSize = this.selectSize.bind(this);
+    this.selectQuantity = this.selectQuantity.bind(this);
   }
 
   selectSize(size) {
     console.log(`selecting size: ${size}`);
-    this.setState({ current_sku: this.props.sizeInfo[size].sku, });
+    this.setState({
+      current_sku: this.props.sizeInfo[size].sku,
+      current_size: size
+    });
+  }
+
+  selectQuantity(qty) {
+    if (this.state.current_size) {
+      console.log(`selecting quantity: ${qty}`);
+      this.setState({ current_quantity: qty })
+    }
   }
 
   render() {
     const sizeOptions = Object.keys(this.props.sizeInfo);
-    const maxQuantity = this.state.current_sku ? this.props.currentStyle.skus[this.state.current_sku].quantity : 0;
+    let maxQuantity = this.state.current_sku ? this.props.currentStyle.skus[this.state.current_sku].quantity : 0;
+    maxQuantity = maxQuantity <= 15 ? maxQuantity : 15;
     return (
       <div className="add-to-cart">
         <SizeSelector sizeOptions={sizeOptions} selectSize={this.selectSize} />
-        <QuantitySelector maxQuantity={maxQuantity} />
+        <QuantitySelector maxQuantity={maxQuantity} selectQuantity={this.selectQuantity} />
         <button className="add-to-cart-button">Add to Cart</button>
         <ClickableStar />
       </div>
--- client/src/components/overview/QuantitySelector.jsx
+++ client/src/components/overview/QuantitySelector.jsx
@@ -1,7 +1,15 @@
 export default function(props) {
+  let options = [];
+  for (let i = 1; i <= props.maxQuantity; i++) {
+    options.push(<option key={i}>{i}</option>);
+  }
+
   return (
-    <select className="quantity-selector">
-      <option default>1</option>
+    <select
+      className="quantity-selector"
+      onChange={(e) => props.selectQuantity(parseInt(e.target.value))}
+    >
+      {options.length ? options : <option>1</option>}
     </select>
   );
 }
