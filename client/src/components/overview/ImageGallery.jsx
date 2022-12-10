3d883385829c40314499017417bd22f5f7998abf
Nate Foster <nate.s.foster@gmail.com>
2022-12-10 09:44:06 -1000

    click thumbnail to change main image

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -6,27 +6,47 @@ export default class ImageGallery extends React.Component {
     this.state = {
       currentImage: {}
     }
+    this.changeMainImage = this.changeMainImage.bind(this);
+    this.handleThumbnailClick = this.handleThumbnailClick.bind(this);
+  }
+
+  changeMainImage(image) {
+    this.setState({ currentImage: image });
+  }
+
+  handleThumbnailClick(e) {
+    if (e.target.className === 'thumbnails-container') return;
+    if (e.target.className === 'thumbnail-img') {
+      console.log(e.target.parentElement.dataset.index);
+      this.changeMainImage(this.props.currentStyle.photos[e.target.parentElement.dataset.index]);
+    } else {
+      console.log(e.target.dataset.index);
+      this.changeMainImage(this.props.currentStyle.photos[e.target.dataset.index]);
+    }
   }
 
   render() {
-    let currentImage = this.props.currentStyle.photos ? this.props.currentStyle.photos[0] : {};
+    let currentImage = this.state.currentImage;
+    if (!currentImage.url) {
+      currentImage = this.props.currentStyle.photos ? this.props.currentStyle.photos[0] : {};
+    }
     return (
       <div className="image-gallery">
         <div className="main-image-container">
           <img src={currentImage.url} alt={this.props.currentStyle.name}></img>
 
-          <div className="thumbnails-container">
+          <div className="thumbnails-container" onClick={this.handleThumbnailClick}>
             {this.props.currentStyle.photos && this.props.currentStyle.photos.map((photo, i) => {
               if (i === this.props.currentStyle.photos.indexOf(currentImage)) {
                 return (
-                  <div className="img-gallery-thumbnail selected-thumbnail" key={i}>
-                    <img src={photo.thumbnail_url}></img>
+                  <div className="img-gallery-thumbnail selected-thumbnail" key={i} data-index={i}>
+                    <img className="thumbnail-img" src={photo.thumbnail_url}></img>
                   </div>
                 );
               } else {
                 return (
-                  <div className="img-gallery-thumbnail" key={i}>
-                    <img src={photo.thumbnail_url}></img>
+                  <div className="img-gallery-thumbnail" key={i} data-index={i}>
+                    <img className="thumbnail-img" src={photo.thumbnail_url}></img>
                   </div>
                 );
               }
