a72df1955bf49d2a51cb41bf395e72943df79639
Nate Foster <nate.s.foster@gmail.com>
2022-12-14 10:10:15 -1000

    deleted helper function from Overview test file

--- testing/src/Overview.test.js
+++ testing/src/Overview.test.js
@@ -1,19 +1,4 @@
-import { roundToNearestQuarter } from '../../helpers/helpers.js';
 import { unmountComponentAtNode } from 'react-dom';
-
-/***** Unit Tests for roundToNearestQuarter helper function *****/
-
-describe('roundToNearestQuarter', function() {
-  it('should round numbers to nearest quarter of a point', function() {
-    expect(roundToNearestQuarter(3)).toBe(3);
-    expect(roundToNearestQuarter(1.25)).toBe(1.25);
-    expect(roundToNearestQuarter(2.75)).toBe(2.75);
-    expect(roundToNearestQuarter(2.73)).toBe(2.75);
-    expect(roundToNearestQuarter(3.402342)).toBe(3.5);
-    expect(roundToNearestQuarter(3.12)).toBe(3);
-    expect(roundToNearestQuarter(3.13)).toBe(3.25);
-  });
-});
 
 /***** React Component Tests *****/
 
