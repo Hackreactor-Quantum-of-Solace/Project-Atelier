3f91a43c01407b41377f9dd9c38b7b14704f940d
debrazhang <debrazhang09@gmail.com>
2022-12-28 15:35:37 -0800

    add carousel to outfitlist

--- client/src/components/relatedItems/OutfitList.jsx
+++ client/src/components/relatedItems/OutfitList.jsx
@@ -10,37 +10,70 @@ export default class OutfitList extends React.Component {
     this.state = {
       icon: 'delete',
       outfitList: getOutfitListInCookie(),
+      outfitListLength: getOutfitListInCookie().length,
+      // need to optimize(both list use carousel)
+      arrowLeft: false,
+      arrowRight: true
+
     }
     this.handleClick = this.handleClick.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    // need to optimize(both list use carousel)
+    this.slider = React.createRef();
+    this.goLeft = this.goLeft.bind(this);
+    this.goRight = this.goRight.bind(this);
   }
+
   handleClick() {
     //use helper function to save customer specific outfitList into cookie
     addOutfitListToCookie(this.props.currentProductId);
     this.setState({outfitList: getOutfitListInCookie()});
+
   }
   handleDelete(clickedId) {
-    this.setState({outfitList:deleteOutfitIdInCookie(clickedId)});
+    this.setState({outfitList: deleteOutfitIdInCookie(clickedId)})
   }
+
+   // need to optimize(both list use carousel)
+  goLeft() {
+    //each card width is 208px, each time move 1 card
+    this.slider.current.scrollLeft += 0.25 * this.slider.current.offsetWidth;
+    this.setState({arrowLeft: true});
+  }
+  goRight() {
+    this.slider.current.scrollLeft -= 0.25 * this.slider.current.offsetWidth;
+    if (this.slider.current.scrollLeft === 0) {
+      this.setState({arrowLeft: false})
+    }
+
+  }
+
   render() {
     return (
-      <div className="outfit-list">
+      <div className="products-list">
         <h2>YOUR OUTFIT</h2>
-        <div className="outfit-container">
+        <div className="related-carousel">
 
-          <div className="card" onClick={this.handleClick}>
-           <h1>Add to Outfit</h1>
-           <button >&#43;</button>
+          <button style={this.state.arrowLeft ? {"visibility":"visible"} : {"visibility":"hidden"}} onClick={this.goRight}>
+            <h1>&#8249;</h1>
+          </button >
+            <div className="add-outfit" onClick={this.handleClick}>
+             <h1>Add to Outfit</h1>
+             <button className="add-button" ><span>&#43;</span></button>
+            </div>
+          <div className="related-container" ref={this.slider}>
+
+
+              {(this.state.outfitList !== undefined) ?
+              this.state.outfitList.map((item, index) => (
+                <SingleCard icon={this.state.icon} id={item} key={item} handleDelete={this.handleDelete}/>
+              ))
+              : null}
+
           </div>
-
-          <div>
-            {(this.state.outfitList !== undefined) ?
-            this.state.outfitList.map((item, index) => (
-              <SingleCard icon={this.state.icon} id={item} key={index} handleDelete={this.handleDelete}/>
-            ))
-            : null}
-
-          </div>
+          <button style={this.state.arrowRight ? {"visibility":"visible"} : {"visibility":"hidden"}} onClick={this.goLeft}>
+            <h1>&#8250;</h1>
+          </button>
         </div>
       </div>
     )
--- client/src/components/relatedItems/RelatedProductsList.jsx
+++ client/src/components/relatedItems/RelatedProductsList.jsx
@@ -29,15 +29,15 @@ export default class RelatedProductsList extends React.Component {
 
   render() {
     return (
-      <div className= "related-products-list">
+      <div className= "products-list">
         <h2>RELATED PRODUCTS</h2>
         <div className="related-carousel">
           <button style={this.state.arrowLeft ? {"visibility":"visible"} : {"visibility":"hidden"}} onClick={this.goRight}>
             <h1>&#8249;</h1>
           </button >
-          <div className="related-items-container" ref={this.slider} >
+          <div className="related-container" ref={this.slider} >
             {this.props.relatedItemsId.map((id, index) => (
-              <SingleCard key={index} id={id} icon={this.state.icon} currentProductFeature={this.props.currentProductFeature} currentProductName={this.props.currentProductName} />
+              <SingleCard key={id} id={id} icon={this.state.icon} currentProductFeature={this.props.currentProductFeature} currentProductName={this.props.currentProductName} />
             ))}
           </div>
           <button style={this.state.arrowRight ? {"visibility":"visible"} : {"visibility":"hidden"}} onClick={this.goLeft}>
