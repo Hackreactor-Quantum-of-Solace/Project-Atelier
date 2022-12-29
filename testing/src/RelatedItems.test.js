24077f2970ec2ca8d742ea827ac622904b6ff205
debrazhang <debrazhang09@gmail.com>
2022-12-29 11:06:15 -0800

    add helper cookie function test case

--- testing/src/RelatedItems.test.js
+++ testing/src/RelatedItems.test.js
@@ -1,3 +1,11 @@
+let cookie = ""
+Object.defineProperty(document, 'cookie', {
+  get: () => cookie,
+  set: (str) => {
+    cookie = str
+  }
+});
+
 import React from "react";
 /* snapshot test way
 import * as rend from "react-test-renderer";
@@ -64,7 +72,10 @@ describe("RelatedItems", () => {
 
 
 
-describe("helper function cookie part1 getOutfitListInCookie", () => {
+describe("helper function cookie getOutfitListInCookie", () => {
+  beforeAll(() => {
+    cookie = "";
+  })
 
   it("should not get cookie named 'outfitList' if it does not exist", () => {
     document.cookie = "newName=[]; ";
@@ -75,7 +86,54 @@ describe("helper function cookie part1 getOutfitListInCookie", () => {
   it("should get specific cookie by name 'outfitList' ", () => {
     document.cookie = "outfitList=[2,1]; ";
     expect(getOutfitListInCookie()).toEqual([2,1]);
+
   })
 })
 
 
+describe("helper function cookie addOutfitListToCookie", () => {
+  beforeAll(() => {
+    cookie = "";
+  })
+
+  it("should add cookie named 'outfitList' if it does not exist", () => {
+    addOutfitListToCookie(5);
+    let outfitList = getOutfitListInCookie();
+    expect(outfitList).toEqual([5]);
+  })
+
+  it("should not add same cookie value ", () => {
+    addOutfitListToCookie(5);
+    let outfitList = getOutfitListInCookie();
+    expect(outfitList).toEqual([5]);
+  })
+  it("should add new cookie value if it is new one", () => {
+    addOutfitListToCookie(6);
+    let outfitList = getOutfitListInCookie();
+    expect(outfitList).toEqual([5,6]);
+  })
+  it("should add new cookie value if it is new one", () => {
+    addOutfitListToCookie(6);
+    let outfitList = getOutfitListInCookie();
+    expect(outfitList).toEqual([5,6]);
+  })
+  it("should directly add array if input is an array", () => {
+    addOutfitListToCookie([1,2]);
+    let outfitList = getOutfitListInCookie();
+    expect(outfitList).toEqual([1,2]);
+  })
+
+})
+
+describe("helper function cookie deleteOutfitIdInCookie", () => {
+
+  beforeAll(() => {
+    cookie = "";
+  })
+
+  it("should add cookie named 'outfitList' if it does not exist", () => {
+    document.cookie = "outfitList=[2,1,3]; ";
+    let outfitList = deleteOutfitIdInCookie(1);
+    expect(outfitList).toEqual([2,3]);
+  })
+})
