eb298b57b6fc86fb4fedcb7854e0356f05344485
MA Hereford <bluechocolate2019@gmail.com>
2023-01-06 12:27:02 -0800

    cleaned up  console logs

--- client/src/components/ratingsReviews/RatingsReviews.jsx
+++ client/src/components/ratingsReviews/RatingsReviews.jsx
@@ -15,28 +15,28 @@ export default class RatingsReviews extends React.Component {
       reviews: [],
       visibleReviews: [],
       averageRating: 0,
-      sortValue: 'relevance',
-      end: 2
+      end: 2,
+      filter: 'relevant',
+      helpfulness: 0,
+      helpfulnessClickedCount: 0,
+
     };
     this.getRatingsReviews = this.getRatingsReviews.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleMoreReviews = this.handleMoreReviews.bind(this);
     this.increaseHelpfulnessCount = this.increaseHelpfulnessCount.bind(this);
-
+    this.handleSort = this.handleSort.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleListMoreReviews = this.handleListMoreReviews.bind(this);
   }
 
   componentDidMount () {
     this.getRatingsReviews(this.props.productId);
   }
 
-  getRatingsReviews(id) {
-
+  getRatingsReviews(id, filter) {
     let config = {
-      url: `/reviews?product_id=${id}`,
+      url: `/reviews?product_id=${id}&sort=${filter}`,
       method: 'get'
     };
-
     axios(config)
       .then ( (reviews) => {
         var totalRating = 0;
@@ -44,13 +44,11 @@ export default class RatingsReviews extends React.Component {
         for (var i = 0; i < reviews.data.results.length; i++) {
           totalRating = totalRating + reviews.data.results[i].rating
         }
-
         this.setState({
           reviews: reviews.data.results,
           visibleReviews: reviews.data.results.slice(0,this.state.end),
           averageRating: totalRating/reviews.data.results.length
         });
-
       })
         .catch( (err) => {
         console.log(err);
@@ -59,6 +57,16 @@ export default class RatingsReviews extends React.Component {
 
   increaseHelpfulnessCount(reviewId, helpfulnessCount) {
 
+    // console.log(this.state.helpfulness, 'line 64')
+    // console.log(this.state.helpfulnessClickedCount, 'line 65')
+    // this.state.helpfulnessClickedCount = this.state.helpfulnessClickedCount + 1;
+    // console.log(this.state.helpfulnessClickedCount, 'line 67')
+
+    // this.setState({
+    //   helpfulness: helpfulnessCount + 1,
+    //   // helpfulnessClickedCount: this.state.helpfulnessClickedCount
+    // });
+
     let config = {
       url: `/reviews/${reviewId}/helpful`,
       method: 'put',
@@ -66,27 +74,25 @@ export default class RatingsReviews extends React.Component {
         helpfulnessCount: helpfulnessCount++
       }
     };
-
     axios(config)
       .then (
-       this.getRatingsReviews(this.props.productId)
+        this.getRatingsReviews(this.props.productId)
       )
       .catch( (err) => {
         console.log(err);
       });
   }
 
-  handleChange(event) {
-    this.setState({sortValue: event.target.value}); //setState is asynch
-    console.log('sorting by ' + this.state.sortValue);
+  handleSort(event) {
+    this.setState({filter: event.target.value}); //setState is asynch
+    this.getRatingsReviews(this.props.productId, event.target.value)
   }
 
   handleSubmit(event) {
     event.preventDefault();
-
   }
 
-  handleMoreReviews(event) {
+  handleListMoreReviews(event) {
     event.preventDefault();
     //rendering 2 additional reviews
     this.state.end = this.state.end + 2;
@@ -96,36 +102,44 @@ export default class RatingsReviews extends React.Component {
   }
 
   render() {
-    return (
-      <div className='reviews-section-container'>
-        <div className='reviews-header'>
-        <h4>RATINGS & REVIEWS</h4>
+    // console.log(this.state.helpfulnessClickedCount, 'line 110')
+    // console.log(this.state.helpfulness, 'line 111')
+    // if (this.state.helpfulnessClickedCount <= 1) {
+      return (
+        <div className='reviews-section-container'>
+          <div className='reviews-header'>
+          <h4>RATINGS & REVIEWS</h4>
+          </div>
+         <div className='ratings-container'>
+            <span><h1>{this.state.averageRating}</h1> <AverageRating averageRating={this.state.averageRating}/></span>
+         </div>
+         <div className='reviews-container'>
+         <form onSubmit={this.handleSubmit}>
+          <label>
+            {this.state.reviews.length} reviews, sorted by
+            <select value={this.state.value} onChange={this.handleSort}>
+              <option value="relevant">relevant</option>
+              <option value="newest">newest</option>
+              <option value="helpful">helpful</option>
+            </select>
+          </label>
+         </form>
+         <ReviewsList
+           review={this.state.reviews}
+           visibleReviews={this.state.visibleReviews}
+           helpfulness={this.state.helpfulness}
+           increaseHelpfulnessCount={this.increaseHelpfulnessCount}
+         />
+         </div>
+         <div>
+          <button onClick={this.handleListMoreReviews}>
+            More Reviews
+          </button>
+         </div>
+         <NewReviewForm />
         </div>
+      )
+    // }
 
-       <div className='ratings-container'>
-          <span><h1>{this.state.averageRating}</h1> <AverageRating averageRating={this.state.averageRating}/></span>
-       </div>
-
-       <div className='reviews-container'>
-       <form onSubmit={this.handleSubmit}>
-        <label>
-          {this.state.reviews.length} reviews, sorted by
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="date">date</option>
-            <option value="helpfulness">helpfulness</option>
-            <option value="relevance">relevant</option>
-          </select>
-        </label>
-       </form>
-       <ReviewsList review={this.state.reviews} visibleReviews={this.state.visibleReviews} value={this.state.sortValue} increaseHelpfulnessCount={this.increaseHelpfulnessCount}/>
-       </div>
-       <div>
-        <button onClick={this.handleMoreReviews}>
-          More Reviews
-        </button>
-       </div>
-       <NewReviewForm />
-      </div>
-    )
   }
 }
